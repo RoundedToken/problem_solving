923a94b9f4114b5ec4c8b7de3c30cddafabd87e1
RoundedToken <117864556+RoundedToken@users.noreply.github.com>
2023-11-10 10:22:04 +0100

    Хэши для строк

--- yandex_problems_4.0/Хэши для строк/D. Кубики в зеркале.js
+++ yandex_problems_4.0/Хэши для строк/D. Кубики в зеркале.js
@@ -3,68 +3,131 @@ const solution = (input) => {
     cubes = cubes.trim().split(' ');
     const maxCount = cubes.length;
     const minCount = Math.ceil(cubes.length / 2);
-    // cubes = cubes.concat(cubes);
     const result = [];
-    const X = 1000001;
-    const mod = 1000000007;
-    const x = [1];
-    const prefix = [+cubes[0]];
-    const suffix = [+cubes[cubes.length - 1]];
 
-    console.log(cubes, 'maxCount=', maxCount, 'minCount=', minCount);
+    const prefix = [BigInt(cubes[0])];
+    const suffix = [BigInt(cubes[cubes.length - 1])];
+    const x = [BigInt(1)];
+    const mod = BigInt(1000000000000007);
+    const X = BigInt(1000001);
 
     for (let i = 1; i < cubes.length; i++) {
-        prefix[i] = (prefix[i - 1] * X + +cubes[i]) % mod;
+        prefix[i] = (prefix[i - 1] * X + BigInt(cubes[i])) % mod;
         x[i] = (x[i - 1] * X) % mod;
     }
 
     for (let i = cubes.length - 2; i >= 0; i--) {
-        suffix.push((suffix.at(-1) * X + +cubes[i]) % mod);
+        suffix.push((suffix.at(-1) * X + BigInt(cubes[i])) % mod);
     }
-
-    // console.log('prefix', prefix);
-    // console.log('suffix', suffix);
-    // console.log('x', x);
 
     function isPalindrome(from, to) {
         const l = to - from + 1;
-        // console.log((prefix[to] + (suffix[cubes.length - 2 - to] ?? 0) * (x[l] ?? 0)) % mod);
-        // console.log(
-        //     (suffix[cubes.length - 1 - from] + (prefix[from - 1] ?? 0) * (x[l] ?? 0)) % mod
-        // );
+
         return (
-            (prefix[to] + (suffix[cubes.length - 2 - to] ?? 0) * (x[l] ?? 0)) % mod ===
-            (suffix[cubes.length - 1 - from] + (prefix[from - 1] ?? 0) * (x[l] ?? 0)) % mod
+            (prefix[to] + (suffix[cubes.length - 2 - to] ?? BigInt(0)) * (x[l] ?? BigInt(0))) %
+                mod ===
+            (suffix[cubes.length - 1 - from] +
+                (prefix[from - 1] ?? BigInt(0)) * (x[l] ?? BigInt(0))) %
+                mod
         );
     }
 
-    for (let halfL = minCount; halfL <= maxCount; halfL++) {
+    for (let halfL = minCount; halfL < maxCount; halfL++) {
         const from = 0;
         const to = (cubes.length - halfL) * 2 - 1;
-        console.log(cubes.slice(from, to + 1));
+
         if (isPalindrome(from, to)) {
             result.push(halfL);
         }
     }
 
     result.push(maxCount);
-    console.log(isPalindrome(0, 5));
+
     return result.join(' ');
 };
 
+const worked = (input) => {
+    let [params, cubes] = input.trim().split('\n');
+    cubes = cubes.trim().split(' ');
+    const maxCount = cubes.length;
+    const minCount = Math.ceil(cubes.length / 2);
+    const result = [];
+
+    const isPalindrome = (from, to) => {
+        const arr = cubes.slice(from, to + 1);
+        const l = arr.length;
+        const isOdd = l % 2 === 1;
+        const mid = Math.floor(l / 2);
+        const left = arr.slice(0, mid);
+        const right = arr.slice(isOdd ? mid + 1 : mid);
+
+        return left.join('') === right.reverse().join('');
+    };
+
+    for (let halfL = minCount; halfL <= maxCount; halfL++) {
+        const from = 0;
+        const to = (cubes.length - halfL) * 2 - 1;
+
+        if (isPalindrome(from, to)) {
+            result.push(halfL);
+        }
+    }
+
+    return result.join(' ');
+};
+
+function generateInput() {
+    const MAX_CUBES = 100;
+    const MAX_COLORS = 100;
+
+    let numCubes = Math.floor(Math.random() * MAX_CUBES) + 1;
+    let numColors = Math.floor(Math.random() * MAX_COLORS) + 1;
+
+    let input = `${numCubes} ${numColors}\n`;
+
+    for (let i = 0; i < numCubes; i++) {
+        input += Math.floor(Math.random() * numColors + 1) + ' ';
+    }
+
+    return input;
+}
+
 function main() {
-    console.log(
-        solution(`6 2
-    1 1 2 2 1 1`)
-    );
-    console.log(
-        solution(`6 2
-    1 1 1 1 1 1`)
-    );
-    console.log(
-        solution(`6 2
-    1`)
-    );
+    let count = 0;
+    const limit = 50000;
+    for (let i = 0; i < limit; i++) {
+        const input = generateInput();
+        if (solution(input) !== worked(input)) {
+            count++;
+        }
+    }
+    console.log((count / limit) * 100, '%');
+    // console.log(
+    //     solution(`100 239
+    // 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17`)
+    // );
+    // console.log(
+    //     worked(`100 239
+    // 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17 17`)
+    // );
+    // console.log(
+    //     solution(`6 2
+    // 1 1 2 2 1 1`),
+    //     worked(`6 2
+    // 1 1 2 2 1 1`)
+    // );
+    // console.log(
+    //     solution(`6 2
+    // 1 1 1 1 1 1`),
+    //     worked(`6 2
+    // 1 1 1 1 1 1`)
+    // );
+    // console.log(
+    //     solution(`6 2
+    // 1`),
+    //     worked(`6 2
+    // 1`)
+    // );
 }
 if (require.main === module) {
     main();
