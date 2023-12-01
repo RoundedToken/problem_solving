1242480cbd44a9af33c0302702335511fea3487c
RoundedToken <117864556+RoundedToken@users.noreply.github.com>
2023-12-01 09:59:21 +0100

    LeetCode: 1662

---
+++ leetcode/easy/1662. Check If Two String Arrays are Equivalent.js
@@ -0,0 +1,5 @@
+// https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/?envType=daily-question&envId=2023-12-01
+
+var arrayStringsAreEqual = function (word1, word2) {
+    return word1.join('') === word2.join('');
+};
