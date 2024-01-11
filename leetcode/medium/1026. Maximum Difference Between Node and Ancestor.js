32d16b84f63dac464e5fe4d7007c25349c6cf4e0
RoundedToken <117864556+RoundedToken@users.noreply.github.com>
2024-01-11 20:28:23 +0100

    LeetCode: 1026

---
+++ leetcode/medium/1026. Maximum Difference Between Node and Ancestor.js
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/description/?envType=daily-question&envId=2024-01-11
+
+var maxAncestorDiff = function (root) {
+    if (root == null) {
+        return 0;
+    }
+
+    let result = 0;
+    helper(root, root.val, root.val);
+
+    function helper(root, max, min) {
+        if (root == null) {
+            result = Math.max(result, max - min);
+            return;
+        }
+
+        max = Math.max(max, root.val);
+        min = Math.min(min, root.val);
+
+        helper(root.left, max, min);
+        helper(root.right, max, min);
+    }
+
+    return result;
+};
