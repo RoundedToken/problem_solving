0113da8aefe0973858bcd8a135c6c11ea2b7de13
RoundedToken <117864556+RoundedToken@users.noreply.github.com>
2024-01-18 20:03:46 +0100

    LeetCode: 70

---
+++ leetcode/easy/70. Climbing Stairs.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/climbing-stairs/description/?envType=daily-question&envId=2024-01-18
+
+var climbStairs = function (n) {
+    let dp = new Array(n + 1);
+    (dp[1] = 1), (dp[2] = 2);
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+};
