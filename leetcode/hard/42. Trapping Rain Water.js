aeb03215979a5ee1caacebb70246aa8928c707f8
RoundedToken <stepan.zip.07@gmail.com>
2024-04-12 07:20:24 +0200

    LeetCode: 42

---
+++ leetcode/hard/42. Trapping Rain Water.js
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/trapping-rain-water/description/?envType=daily-question&envId=2024-04-12
+
+var trap = function (height) {
+    let max1 = 0;
+    let left = [height.length];
+    for (let i = 0; i < height.length; i++) {
+        if (max1 < height[i]) {
+            max1 = height[i];
+        }
+        left[i] = max1;
+    }
+
+    let max2 = 0;
+
+    let right = [height.length];
+    for (let i = height.length - 1; i >= 0; i--) {
+        if (max2 < height[i]) {
+            max2 = height[i];
+        }
+        right[i] = max2;
+    }
+
+    let trap = 0;
+    for (let i = 0; i < height.length; i++) {
+        trap += Math.min(left[i], right[i]) - height[i];
+    }
+    return trap;
+};
