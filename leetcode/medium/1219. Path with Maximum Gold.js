d7e48cf0a12591d06acdd0642627473db295e1a9
RoundedToken <stepan.zip.07@gmail.com>
2024-05-14 06:57:59 +0200

    LeetCode: 1219

---
+++ leetcode/medium/1219. Path with Maximum Gold.js
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/path-with-maximum-gold/description/?envType=daily-question&envId=2024-05-14
+
+var getMaximumGold = function (grid) {
+    const m = grid.length;
+    const n = grid[0].length;
+    let maxGold = 0;
+    function dfs(row, col) {
+        if (row < 0 || col < 0 || row >= m || col >= n || grid[row][col] === 0) {
+            return 0;
+        }
+        let currenGold = grid[row][col];
+        grid[row][col] = 0;
+        const down = dfs(row + 1, col);
+        const up = dfs(row - 1, col);
+        const right = dfs(row, col + 1);
+        const left = dfs(row, col - 1);
+        grid[row][col] = currenGold;
+        return currenGold + Math.max(up, down, right, left);
+    }
+
+    for (let row = 0; row < m; row++) {
+        for (let col = 0; col < n; col++) {
+            if (grid[row][col] > 0) {
+                maxGold = Math.max(maxGold, dfs(row, col));
+            }
+        }
+    }
+    return maxGold;
+};
