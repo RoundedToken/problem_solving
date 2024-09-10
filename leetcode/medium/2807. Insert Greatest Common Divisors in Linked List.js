360d888760c30b73ec3efbc907ea08c926af0157
RoundedToken <stepan.zip.07@gmail.com>
2024-09-10 11:24:38 +0200

    LeetCode: 2807

---
+++ leetcode/medium/2807. Insert Greatest Common Divisors in Linked List.js
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/insert-greatest-common-divisors-in-linked-list/description/
+
+var gcd = function (a, b) {
+  if (!b) {
+    return a;
+  }
+
+  return gcd(b, a % b);
+};
+var insertGreatestCommonDivisors = function (head) {
+  const ans = head;
+  let l = head;
+
+  while (l && l.next) {
+    l.next = new ListNode(gcd(l.val, l.next.val), l.next);
+    l = l.next.next;
+  }
+
+  return ans;
+};
