d00c7e1c056a406773ee45184870df64abf039fc
RoundedToken <stepan.zip.07@gmail.com>
2024-09-13 10:41:36 +0200

    LeetCode: 1310

---
+++ leetcode/medium/1310. XOR Queries of a Subarray.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/xor-queries-of-a-subarray/description/
+
+var xorQueries = function (arr, queries) {
+  const res = [];
+  const xorResults = [];
+  xorResults[0] = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    xorResults.push(arr[i] ^ xorResults[i - 1]);
+  }
+
+  for (const [start, end] of queries) {
+    res.push(xorResults[start] ^ xorResults[end] ^ arr[start]);
+  }
+
+  return res;
+};
