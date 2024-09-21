9d2e73f76b1b598e4aaaeee99a64b12a41823aee
RoundedToken <stepan.zip.07@gmail.com>
2024-09-21 09:41:53 +0200

    LeetCode: 386

---
+++ leetcode/medium/386. Lexicographical Numbers.js
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/lexicographical-numbers/description/
+
+var lexicalOrder = function (n) {
+  const arr = [];
+
+  function dfs(baseIndex) {
+    if (baseIndex * 10 > n) {
+      return;
+    }
+
+    for (let i = baseIndex * 10; i < baseIndex * 10 + 10 && i <= n; i++) {
+      arr.push(i);
+      dfs(i);
+    }
+  }
+
+  let stack = [];
+
+  for (let i = 1; i <= 9 && i <= n; i++) {
+    arr.push(i);
+    dfs(i);
+  }
+
+  return arr;
+};
